bb3d55ee2ec2764adc5f7c83246f793fae347c61
shujath-git900 <shujath.devops@gmail.com>
2026-01-28 12:47:44 +0530

    variables demo

--- 03-variables/vars.tf
+++ 03-variables/vars.tf
@@ -5,9 +5,21 @@ variable "x" {
 variable "x_list" {
   default = [1,2,3]
 }
+variable "y_map" {
+  default = {
+    x = "ABC",
+    y = "XYZ"
+  }
+}
 output "x" {
   value = var.x
 }
 output "x_list" {
   value = var.x_list
+}
+output "x_1" {
+  value = var.x_list[0]
+}
+output "y_map" {
+  value = var.y_map["y"]
 }
