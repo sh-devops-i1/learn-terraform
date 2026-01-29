e75cbe3da8e606bbab3ba7002cbe0d2009f43e72
shujath-git900 <shujath.devops@gmail.com>
2026-01-29 15:03:42 +0530

    exit 1 added

--- 08-multi-env/run.sh
+++ 08-multi-env/run.sh
@@ -3,9 +3,11 @@ action=$2
 
 if [ -z "${env}" ]; then
     echo -e "\e[31mInput missing Environment\e[0m"
+    exit 1
 fi
 if [ -z "${action}" ]; then
     echo -e "\e[31mInsert apply|destroy\e[0m"
+    exit 1
 fi
 
 rm -rf .terraform/terraform.tfstate
